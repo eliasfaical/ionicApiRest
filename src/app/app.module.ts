b3a734d9a90bd40f55246b49bc11f201f0f62eb4
Elias Faiçal <eliasfaical@gmail.com>
2018-01-03 15:20:35 -0200

    Criando paginas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,17 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { HttpModule } from '@angular/http';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { UsersProvider } from '../providers/user/user';
+
+import { CreateAccountPageModule } from './../pages/create-account/create-account.module';
+import { LoginPageModule } from './../pages/login/login.module';
+import { UserListPageModule } from './../pages/user-list/user-list.module';
+import { UserEditPageModule } from './../pages/user-edit/user-edit.module';
+import { UserDetailPageModule } from './../pages/user-detail/user-detail.module';
 
 @NgModule({
   declarations: [
@@ -14,6 +22,12 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
+    CreateAccountPageModule,
+    LoginPageModule,
+    UserEditPageModule,
+    UserDetailPageModule,
+    UserListPageModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -24,7 +38,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    UsersProvider
   ]
 })
 export class AppModule {}
---
+++ src/pages/create-account/create-account.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
+import { UsersProvider } from '../../providers/user/user';
+
+@IonicPage()
+@Component({
+  selector: 'page-create-account',
+  templateUrl: 'create-account.html',
+})
+export class CreateAccountPage {
+
+  model: User;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private toast: ToastController, private userProvider: UsersProvider) {
+    this.model = new User();
+    this.model.email = '';
+    this.model.password = '';
+  }
+
+  createAccount() {
+    this.userProvider.createAccount(this.model.email, this.model.password)
+      .then((result: any) => {
+        this.toast.create({ message: 'Usuário criado com sucesso. Token: ' + result.token, position: 'botton', duration: 3000 }).present();
+
+        //Salvar o token no Ionic Storage para usar em futuras requisições.
+        //Redirecionar o usuario para outra tela usando o navCtrl
+        //this.navCtrl.pop();
+        //this.navCtrl.setRoot()
+      })
+      .catch((error: any) => {
+        this.toast.create({ message: 'Erro ao criar o usuário. Erro: ' + error.error, position: 'botton', duration: 3000 }).present();
+      });
+  }
+
+}
+
+export class User {
+  email: string;
+  password: string;
+}
